e18afe07dbaf06659956823db5d019b9cd8155f2
KouZH1890 <zihong0214@1utar.my>
2026-02-26 05:50:13 +0800

    Change product display

--- src/app/components/products/products.ts
+++ src/app/components/products/products.ts
@@ -11,11 +11,14 @@ export class Products {
   products = [
     { name: 'Basic Tee', price: 'RM39', image: 'assets/t1.jpg', category: 'Tops' },
     { name: 'Oversized Shirt', price: 'RM59', image: 'assets/t2.jpg', category: 'Tops' },
+    { name: 'Ribbed Tank Top', price: 'RM59', image: 'assets/t5.avif', category: 'Tops' },
+    { name: 'Heavyweight Tee', price: 'RM59', image: 'assets/t6.avif', category: 'Tops' },
+    { name: 'Pointelle Knit Top', price: 'RM59', image: 'assets/t7.avif', category: 'Tops' },
     { name: 'Hoodie', price: 'RM89', image: 'assets/t3.png', category: 'Outerwear' },
     { name: 'Jacket', price: 'RM129', image: 'assets/t4.jpg', category: 'Outerwear' },
-    { name: 'Jacket', price: 'RM129', image: 'assets/t4.jpg', category: 'Outerwear' },
-    { name: 'Jacket', price: 'RM129', image: 'assets/t4.jpg', category: 'Outerwear' },
-    { name: 'Jacket', price: 'RM129', image: 'assets/t4.jpg', category: 'Outerwear' },
+    { name: 'Cropped Faux Leather Jacket', price: 'RM129', image: 'assets/t8.avif', category: 'Outerwear' },
+    { name: 'Technical Trench Coat', price: 'RM129', image: 'assets/t9.avif', category: 'Outerwear' },
+    { name: 'Quilted Puffer Bomber', price: 'RM129', image: 'assets/t10.jpg', category: 'Outerwear' },
   ];
 
   selectedCategory: string = 'All';
